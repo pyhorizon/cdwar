b0bb32fefc8a6d2a961162c7d7afb4983a83b367
asailor <2269254303@qq.com>
2019-04-08 22:47:58 +0800

    second

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import torch
 from torchvision.datasets import ImageFolder
 from torchvision import transforms as ts
 from matplotlib import pyplot as plt
+from skimage import io
 #firstly, fit the torch api
 def to_torch(path):
     for c,i in enumerate(os.listdir(path)):
@@ -21,12 +22,20 @@ def show(tensor):
     plt.imshow(img)
     plt.show()
 
-def prep(path,trans_list=[ts.Resize((255,255)),ts.CenterCrop(200),ts.ToTensor()]\
+def prep(path,trans_list=[ts.Resize((255,255)),ts.CenterCrop(224),ts.ToTensor()]\
          ,batch_size=128,shuffle=True):
     transform = ts.Compose(trans_list)
     dataset=ImageFolder(path,transform=transform)
     data=torch.utils.data.DataLoader(dataset,batch_size=batch_size,shuffle=shuffle)
     return data
+
+def to_tensor(x):
+    img=io.imread(x)
+    img.resize(224,224,3)
+    img=img.reshape(1,224,224,3)
+    img2tensor=torch.from_numpy(img).type(torch.float32).cuda()
+    img2tensor=img2tensor.permute(0,3,1,2)
+    return img2tensor
 
 
 
